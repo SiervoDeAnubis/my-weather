67662fc39f59acb910a7e9596ba9940a52c24c83
César Olayo Espíritu <omnislahs@hotmail.com>
2020-02-26 12:34:12 -0600

    addidng ts file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import TodoApp from "./TodoApp";
+
 import "./App.css";
 
 const App = () => (
   <div className="App">
     <h1>Hello World, Todo App</h1>
-    <TodoApp />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ import { reducer } from "./redux/store";
 import "./index.css";
 import App from "./App";
 
-const composeEnhaced = compose(applyMiddleware(logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhaced = compose(
+  applyMiddleware(logger),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const store = createStore(reducer, composeEnhaced);
 
-render(<Provider store={store}><App /></Provider>  , document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
