a5315e5f20cf8de1e764e511beb7e15cc91f19e5
César Olayo Espíritu <gb_cespiritu@idcontractors.net>
2020-02-26 12:10:27 -0600

    split code

---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoForm = props => (
+  <form>
+    <label htmlFor="new_todo"> Add New Todo </label>
+    <input type="text" />
+  </form>
+);
+
+export default TodoForm;
